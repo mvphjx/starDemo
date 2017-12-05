9023a28ef92c5a0383f18ae33c656edca895bba5
511572653@qq.com <511572653@qq.com>
2017-12-05 22:48:09 +0800

    颜色选取控件

---
+++ colorpicker/js/layout.js
@@ -0,0 +1,17 @@
+// (function($){
+// 	var initLayout = function() {
+//
+// 		$('#colorSelector').ColorPicker({
+// 			color: '#7c7ca6',
+//             onSubmit:function(hsb, hex, rgb){
+//
+// 			},
+// 			onChange: function (hsb, hex, rgb) {
+// 				$('#colorSelector div').css('backgroundColor', '#' + hex);
+// 			}
+// 		});
+// 	};
+//
+//
+// 	EYE.register(initLayout, 'init');
+// })(jQuery)
